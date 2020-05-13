ce686617868eba03378d2b1dbd87e1f040e2c0b7
Garrett <g.romo2@students.clark.edu>
2020-05-13 12:15:40 -0700

    Mod 4 Lab 2

--- Template.py
+++ Template.py
@@ -1,6 +1,6 @@
 """
 CTEC 121
-<your name>
+<Garrett>
 <assignment/lab name>
 <assignment/lab description
 """
@@ -10,8 +10,21 @@ Input(s): list/description
 Process: description of what function does
 Output: return value and description
 """
-
+from graphics import *
 def main():
-    # code here
-
+    '''
+    startValue = 1
+    endValue = 10
+    sum = 0
+    if startValue % 2 == 1:
+        startValue = startValue + 1
+    for i in range(startValue, endValue, 2):
+        sum = sum + i
+    print(sum)
+    '''
+    win = GraphWin("demo", 800, 800)
+    win.setCoords(-4.0, -4.0, 4.0, 4.0)
+    p1 = Circle(Point(2, 3), 0.1).setFill("black").draw(win)
+    p2 = Point(-3, 1).draw(win)
+    p3 = Point(-1.5, -2.5).draw(win)
 main()    
